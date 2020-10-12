25de912e12701a68f73923984e7cac5b777b71a1
vladubase <vladubase@gmail.com>
2020-10-12 23:14:50 +0300

    Add: Program example start initialization: LCD1602.

---
+++ Program Examples/LCD1602/Debug/main.d
@@ -0,0 +1,24 @@
+main.d main.o: .././main.c .././inc/main.h \
+ c:\user\ programs\atmel\ studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay.h \
+ c:\user\ programs\atmel\ studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\inttypes.h \
+ c:\user\ programs\atmel\ studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stdint.h \
+ c:\user\ programs\atmel\ studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdint.h \
+ c:\user\ programs\atmel\ studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay_basic.h \
+ c:\user\ programs\atmel\ studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\math.h \
+ c:\user\ programs\atmel\ studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stdbool.h
+
+.././inc/main.h:
+
+c:\user\ programs\atmel\ studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay.h:
+
+c:\user\ programs\atmel\ studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\inttypes.h:
+
+c:\user\ programs\atmel\ studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stdint.h:
+
+c:\user\ programs\atmel\ studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdint.h:
+
+c:\user\ programs\atmel\ studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay_basic.h:
+
+c:\user\ programs\atmel\ studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\math.h:
+
+c:\user\ programs\atmel\ studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stdbool.h:
---
+++ Program Examples/LCD1602/main.c
@@ -0,0 +1,23 @@
+/****
+	*	@name		LCD1602
+	*	@file 		main.c
+	*
+	*	@author 	Uladzislau 'vladubase' Dubatouka
+	*				<vladubase@gmail.com>
+	*	@version	V1.0
+	*	@date 		Created on 2020.10.12
+	*
+*****/
+
+#include "inc/main.h"
+
+
+/**************************************** Main ****************************************/
+
+void main (void) {
+	InitGPIO ();
+	
+	while (true) {
+		
+	}
+}
